d78a431fef71cb045219d18cc73c40d095230f7e
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2023-08-28 02:37:33 -0700

    tweaks to entities

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import BoxModelComponent from "./core/component/types/BoxModelComponent";
 import BoxCollisionComponent from "./core/component/types/BoxCollisionComponent";
 import movementSystem from "./core/system/types/MovementSystem";
 import collisionSystem from "./core/system/types/CollisionSystem";
-import inputSystem from "./core/system/types/InputSystem";
 import { CANVAS_HEIGHT, CANVAS_WIDTH, X, Y, ctx } from "./core/consts";
 import LocationComponent from "./core/component/types/LocationComponent";
 import VelocityComponent from "./core/component/types/VelocityComponent";
 import KeyboardInputComponent from "./core/component/types/KeyboardInputComponent";
+import DestructableComponent from "./core/component/types/DestructableComponent";
 
 const center = [CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2];
 
@@ -23,21 +23,22 @@ const player = new Entity([
     h: 30,
   }),
   new LocationComponent({ x: center[X] - 100, y: CANVAS_HEIGHT - 40 }),
-  new VelocityComponent({ x: 0, y: 0, speed: 3 }),
+  new VelocityComponent({ x: 0, y: 0, speed: 5 }),
   new KeyboardInputComponent({ left: "KeyA", right: "KeyD" }),
 ]);
 
 const ball = new Entity([
   new BoxModelComponent({ color: "blue", w: 10, h: 10 }),
   new BoxCollisionComponent({ w: 10, h: 10 }),
-  new LocationComponent({ x: 0, y: 0 }),
-  new VelocityComponent({ x: 1, y: 1, speed: 2 }),
+  new LocationComponent({ x: 10, y: 10 }),
+  new VelocityComponent({ x: 1, y: 1, speed: 5 }),
 ]);
 
 const brick = new Entity([
   new BoxModelComponent({ color: "yellow", h: 25, w: 50 }),
   new BoxCollisionComponent({ w: 50, h: 25 }),
   new LocationComponent({ x: center[X] - 25, y: center[Y] - 12.5 }),
+  new DestructableComponent(),
 ]);
 
 entityManager.addEntity(player).addEntity(ball).addEntity(brick);
