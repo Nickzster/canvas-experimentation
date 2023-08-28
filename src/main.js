70f67e1bd17c26c09079bbdbfee31a64375deed7
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2023-08-28 00:00:54 -0700

    repurpose main for a breakout type game

--- src/main.js
+++ src/main.js
@@ -4,105 +4,43 @@ import BoxModelComponent from "./core/component/types/BoxModelComponent";
 import BoxCollisionComponent from "./core/component/types/BoxCollisionComponent";
 import movementSystem from "./core/system/types/MovementSystem";
 import collisionSystem from "./core/system/types/CollisionSystem";
+import inputSystem from "./core/system/types/InputSystem";
 import { CANVAS_HEIGHT, CANVAS_WIDTH, X, Y, ctx } from "./core/consts";
 import LocationComponent from "./core/component/types/LocationComponent";
 import VelocityComponent from "./core/component/types/VelocityComponent";
+import KeyboardInputComponent from "./core/component/types/KeyboardInputComponent";
 
 const center = [CANVAS_WIDTH / 2, CANVAS_HEIGHT / 2];
 
-const OFFSET = 30;
+const player = new Entity([
+  new BoxModelComponent({
+    color: "black",
+    w: 120,
+    h: 30,
+  }),
+  new BoxCollisionComponent({
+    w: 120,
+    h: 30,
+  }),
+  new LocationComponent({ x: center[X] - 100, y: CANVAS_HEIGHT - 40 }),
+  new VelocityComponent({ x: 0, y: 0, speed: 5 }),
+  new KeyboardInputComponent({ left: "KeyA", right: "KeyD" }),
+]);
 
-entityManager
-  .addEntity(
-    new Entity([
-      new BoxModelComponent({
-        color: "blue",
-        w: 50,
-        h: 50,
-      }),
-      new BoxCollisionComponent({
-        w: 50,
-        h: 50,
-      }),
-      new LocationComponent({ x: center[X] - OFFSET, y: center[Y] + OFFSET }),
-      new VelocityComponent({ x: -1, y: 1, speed: 1 }),
-    ])
-  )
-  .addEntity(
-    new Entity([
-      new BoxModelComponent({
-        color: "red",
-        w: 50,
-        h: 50,
-      }),
-      new BoxCollisionComponent({
-        w: 50,
-        h: 50,
-      }),
-      new LocationComponent({ x: center[X] + OFFSET, y: center[Y] + OFFSET }),
-      new VelocityComponent({ x: 1, y: 1, speed: 2 }),
-    ])
-  )
-  .addEntity(
-    new Entity([
-      new BoxModelComponent({
-        color: "yellow",
-        w: 50,
-        h: 50,
-      }),
-      new BoxCollisionComponent({
-        w: 50,
-        h: 50,
-      }),
-      new LocationComponent({ x: center[X] - OFFSET, y: center[Y] - OFFSET }),
-      new VelocityComponent({ x: -1, y: -1, speed: 3 }),
-    ])
-  )
-  .addEntity(
-    new Entity([
-      new BoxModelComponent({
-        color: "green",
-        w: 50,
-        h: 50,
-      }),
-      new BoxCollisionComponent({
-        w: 50,
-        h: 50,
-      }),
-      new LocationComponent({ x: center[X] + OFFSET, y: center[Y] - OFFSET }),
-      new VelocityComponent({ x: 1, y: -1, speed: 4 }),
-    ])
-  )
-  .addEntity(
-    new Entity([
-      new BoxModelComponent({
-        color: "purple",
-        w: 50,
-        h: 50,
-      }),
-      new BoxCollisionComponent({
-        w: 50,
-        h: 50,
-      }),
-      new LocationComponent({ x: 1, y: 1 }),
-      new VelocityComponent({ x: 1, y: 0, speed: 5 }),
-    ])
-  )
-  .addEntity(
-    new Entity([
-      new BoxModelComponent({
-        color: "orange",
-        w: 50,
-        h: 50,
-      }),
-      new BoxCollisionComponent({
-        w: 50,
-        h: 50,
-      }),
-      new LocationComponent({ x: CANVAS_WIDTH - 51, y: 1 }),
-      new VelocityComponent({ x: 0, y: 1, speed: 6 }),
-    ])
-  );
+const ball = new Entity([
+  new BoxModelComponent({ color: "blue", w: 10, h: 10 }),
+  new BoxCollisionComponent({ w: 10, h: 10 }),
+  new LocationComponent({ x: 0, y: 0 }),
+  new VelocityComponent({ x: 1, y: 1, speed: 2 }),
+]);
+
+const brick = new Entity([
+  new BoxModelComponent({ color: "yellow", h: 25, w: 50 }),
+  new BoxCollisionComponent({ w: 50, h: 25 }),
+  new LocationComponent({ x: center[X] - 25, y: center[Y] - 12.5 }),
+]);
+
+entityManager.addEntity(player).addEntity(ball).addEntity(brick);
 function draw() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
