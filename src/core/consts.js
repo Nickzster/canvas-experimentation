d2e9cbfbbee3c550f2dbaad851bd36b2b631c5d5
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2023-08-27 23:06:04 -0700

    Add MAX_ENTITIES and vector + location component types

--- src/core/consts.js
+++ src/core/consts.js
@@ -1,6 +1,8 @@
 export const COMPONENT_TYPES = {
   BOX_COLLISION_COMPONENT_TYPE: 0,
   BOX_MODEL_COMPONENT_TYPE: 1,
+  LOCATION_COMPONENT_TYPE: 2,
+  VELOCITY_COMPONENT_TYPE: 3,
 };
 
 export const X = 0;
@@ -11,3 +13,5 @@ export const CANVAS_HEIGHT = 512;
 
 const canvas = document.getElementById("sandbox");
 export const ctx = canvas.getContext("2d");
+
+export const MAX_ENTITIES = 10;
