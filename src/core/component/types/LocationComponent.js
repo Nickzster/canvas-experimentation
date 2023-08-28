f94dce1b20f43c8d2c94d7cff10aa3ceb91cf1db
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2023-08-27 23:07:10 -0700

    Build new location component to represent entity locations

---
+++ src/core/component/types/LocationComponent.js
@@ -0,0 +1,19 @@
+import { COMPONENT_TYPES, X, Y } from "../../consts";
+import Component from "../Component";
+
+export default class LocationComponent extends Component {
+  x;
+  y;
+
+  constructor({ x, y }) {
+    super(COMPONENT_TYPES.LOCATION_COMPONENT_TYPE);
+    this.x = x;
+    this.y = y;
+    this.setAttributes({ x, y });
+  }
+
+  setAttributes({ x, y }) {
+    this.x = x;
+    this.y = y;
+  }
+}
