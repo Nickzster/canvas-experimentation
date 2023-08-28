84918d37a812870b49baa59d40f903d5fa3498eb
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2023-08-28 01:25:12 -0700

    provide a way to update vector

--- src/core/component/types/VelocityComponent.js
+++ src/core/component/types/VelocityComponent.js
@@ -12,6 +12,11 @@ export default class VelocityComponent extends Component {
     this.vec = new Vec2({ x: x * speed, y: y * speed });
   }
 
+  updateVec({ x, y }) {
+    this.vec.update({ x, y });
+    return this;
+  }
+
   update() {
     this.vec.reverse();
   }
