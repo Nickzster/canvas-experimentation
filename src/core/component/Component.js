b4def743d69f751d6300785d5f3eaee68be770fd
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2023-08-29 01:41:24 -0700

    register component when creating so that ids dont need to be managed anymore

--- src/core/component/Component.js
+++ src/core/component/Component.js
@@ -1,11 +1,8 @@
 import componentManager from "./ComponentManager";
 
 export default class Component {
-  tag = "BASE_COMPONENT";
-  id = -1;
-
   constructor(tag) {
-    this.tag = tag;
-    this.id = componentManager.register(tag);
+    this.type = componentManager.register(tag);
+    return this;
   }
 }
