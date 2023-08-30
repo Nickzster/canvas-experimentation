5f3f8c722ab8ef97e69fc9ccda5a017af4c787dc
Nick Zimmermann <nicholas.r.zimmermann@gmail.com>
2023-08-30 03:11:39 -0700

    Update canvas size, set max entities to 100, create collision types object

--- src/core/consts.js
+++ src/core/consts.js
@@ -1,10 +1,15 @@
 export const X = 0;
 export const Y = 1;
 
-export const CANVAS_WIDTH = 768;
-export const CANVAS_HEIGHT = 512;
+export const CANVAS_WIDTH = 1024;
+export const CANVAS_HEIGHT = 768;
 
 const canvas = document.getElementById("sandbox");
 export const ctx = canvas.getContext("2d");
 
-export const MAX_ENTITIES = 10;
+export const MAX_ENTITIES = 100;
+
+export const COLLISION_TYPES = {
+  OBJECT_TO_OBJECT: 0,
+  OUT_OF_BOUNDS: 1,
+};
